48c228023db7894a9dd3bdaa22b79b75f43ea1c9
Paramjit Singh <sparamjit92@gmail.com>
2025-01-30 17:59:23 +0530

    new branch

--- Maui9Bugs/MainPage.cs
+++ Maui9Bugs/MainPage.cs
@@ -4,6 +4,18 @@ public class MainPage : ContentPage
 {
 	public MainPage()
 	{
+
+		MenuBarItem account = new MenuBarItem { Text = "Settings" };
+
+            MenuFlyoutItem changePasswordFlyoutItem = new MenuFlyoutItem { Text = "Profile" };
+          
+
+            MenuFlyoutItem couponMenuFlyout = new MenuFlyoutItem { Text = "Change Password" };
+           
+
+            account.Add(changePasswordFlyoutItem);
+            account.Add(couponMenuFlyout);
+            this.MenuBarItems.Add(account);
 		Content = new VerticalStackLayout
 		{
 			Children = {
